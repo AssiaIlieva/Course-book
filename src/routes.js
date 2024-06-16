9750f80b859958e7387615a089545cf20bf1da2d
AssiaIlieva <ac17@abv.bg>
2024-06-16 21:48:32 +0300

    Add 404 view and functionality

--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,9 @@ const authController = require('./controllers/authController');
 router.use(homeController);
 router.use('/auth', authController);
 
+router.all('*', (req, res) => {
+    res.render('404');
+});
+
 
 module.exports = router;
