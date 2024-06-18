29d3ac4394c883237b14eb42acaf70956a1b339d
AssiaIlieva <ac17@abv.bg>
2024-06-18 13:25:54 +0300

    Added profile functionality

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,11 +1,20 @@
 const router = require('express').Router();
+const { isAuth } = require('../middlewares/authMiddlware');
 const courseService = require('../services/courseService');
+const userService = require('../services/userService')
 
 router.get('/', async (req, res) => {
     const latestCourses = await courseService.getLatest().lean();
-    console.log(latestCourses);
     res.render('home', {latestCourses})
 });
+
+router.get('/profile', isAuth, async(req, res) => {
+    const user = await userService.getInfo(req.user._id).lean();
+    const createdCoursesCount = user.createdCourses?.length || 0;
+    const signUpCoursesCount = user.signedUpCourses?.length || 0
+
+    res.render('profile', {user, createdCoursesCount, signUpCoursesCount});
+})
 
 
 
