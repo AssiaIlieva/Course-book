445d3aef57fc575397fd63cc8e9b0577364029ce
AssiaIlieva <ac17@abv.bg>
2024-06-18 08:20:06 +0300

    Added isCourseOwner guard middleware for delete course functionality

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -38,13 +38,17 @@ router.get('/:courseId/sign-up', async (req, res) => {
     res.redirect(`/courses/${req.params.courseId}/details`)
 });
 
-router.get('/:courseId/delete', async(req, res) => {
+router.get('/:courseId/delete', isCourseOwner, async(req, res) => {
     await courseService.delete(req.params.courseId);
     res.redirect('/courses')
 });
 
 async function isCourseOwner(req, res, next) {
-    const
+    const course = await courseService.getOne(req.params.courseId);
+    if(course.owner != req.user?._id) {
+        return res.redirect(`/courses/${req.params.courseId}/details`);
+    }
+    next();
 }
 
 
